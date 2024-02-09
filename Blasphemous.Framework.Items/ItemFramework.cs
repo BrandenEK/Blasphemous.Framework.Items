160f6255dd1c460fab36dbe111280200bc27fa9a
unknown <brandenek03912@hotmail.com>
2024-02-09 11:19:05 -0500

    core: add api code

--- Blasphemous.Framework.Items/ItemFramework.cs
+++ Blasphemous.Framework.Items/ItemFramework.cs
@@ -1,4 +1,8 @@
 ﻿using Blasphemous.ModdingAPI;
+using Framework.Managers;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Blasphemous.Framework.Items;
 
@@ -6,8 +10,54 @@ public class ItemFramework : BlasMod
 {
     public ItemFramework() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
-    protected override void OnInitialize()
+    protected override void OnAllInitialized()
     {
-        LogError($"{ModInfo.MOD_NAME} has been initialized");
+        foreach (var item in ItemRegister.Items.Where(i => i.AddInventorySlot))
+            AddItemCount(item);
     }
+
+    protected override void OnNewGame()
+    {
+        foreach (var item in ItemRegister.Items.Where(i => i.CarryOnStart))
+            item.GiveItem();
+    }
+
+    private void AddItemCount(ModItem item)
+    {
+        switch (item.ItemType)
+        {
+            case ModItem.ModItemType.RosaryBead:
+                itemCountsByType[InventoryManager.ItemType.Bead] = new Vector2(44, itemCountsByType[InventoryManager.ItemType.Bead].y + 1);
+                return;
+            case ModItem.ModItemType.Prayer:
+                itemCountsByType[InventoryManager.ItemType.Prayer] = new Vector2(17, itemCountsByType[InventoryManager.ItemType.Prayer].y + 1);
+                return;
+            case ModItem.ModItemType.Relic:
+                itemCountsByType[InventoryManager.ItemType.Relic] = new Vector2(7, itemCountsByType[InventoryManager.ItemType.Relic].y + 1);
+                return;
+            case ModItem.ModItemType.SwordHeart:
+                itemCountsByType[InventoryManager.ItemType.Sword] = new Vector2(11, itemCountsByType[InventoryManager.ItemType.Sword].y + 1);
+                return;
+            case ModItem.ModItemType.QuestItem:
+                Main.ItemFramework.LogWarning("Can not add an inventory slot for quest items!");
+                return;
+            case ModItem.ModItemType.Collectible:
+                itemCountsByType[InventoryManager.ItemType.Collectible] = new Vector2(44, itemCountsByType[InventoryManager.ItemType.Collectible].y + 1);
+                return;
+        }
+    }
+
+    public Vector2 GetItemCountOfType(InventoryManager.ItemType itemType)
+    {
+        return itemCountsByType.TryGetValue(itemType, out Vector2 amount) ? amount : new Vector2();
+    }
+
+    private readonly Dictionary<InventoryManager.ItemType, Vector2> itemCountsByType = new()
+    {
+        { InventoryManager.ItemType.Bead, new Vector2(44, 44) },
+        { InventoryManager.ItemType.Prayer, new Vector2(17, 17) },
+        { InventoryManager.ItemType.Relic, new Vector2(7, 7) },
+        { InventoryManager.ItemType.Sword, new Vector2(11, 11) },
+        { InventoryManager.ItemType.Collectible, new Vector2(44, 44) },
+    };
 }
---
+++ Blasphemous.Framework.Items/ItemRegister.cs
@@ -0,0 +1,25 @@
+﻿using Blasphemous.ModdingAPI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blasphemous.Framework.Items;
+
+/// <summary> Registers a new item </summary>
+public static class ItemRegister
+{
+    private static readonly List<ModItem> _items = new();
+    internal static IEnumerable<ModItem> Items => _items;
+
+    /// <summary> Registers a new item </summary>
+    public static void RegisterItem(this ModServiceProvider provider, ModItem item)
+    {
+        if (provider == null)
+            return;
+
+        if (_items.Any(i => i.Id == item.Id))
+            return;
+
+        _items.Add(item);
+        Main.ItemFramework.Log($"Registered custom item: {item.Id}");
+    }
+}
