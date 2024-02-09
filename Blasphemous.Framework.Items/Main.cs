f6f3fe22fd6bb368dce2a2d0b6fd9537b42e032a
unknown <brandenek03912@hotmail.com>
2024-02-09 11:03:57 -0500

    init: add base project files

---
+++ Blasphemous.Framework.Items/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.Framework.Items;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "2.0.2")]
+public class Main : BaseUnityPlugin
+{
+    public static ItemFramework ItemFramework { get; private set; }
+
+    private void Start()
+    {
+        ItemFramework = new ItemFramework();
+    }
+}
